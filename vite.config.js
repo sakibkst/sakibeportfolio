540c38c7e25d8b0cca3d7b7a6a59c96da4db3b61
sakibkst <sakibkst38@gmail.com>
2025-08-12 16:26:17 +0600

    complted

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/sakib_eportfolio/',
+  base: '/sakibeportfolio/',
   plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
